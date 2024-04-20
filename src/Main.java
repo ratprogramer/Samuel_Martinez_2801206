bbe0c646c25d288689ff0a26ef8b7f8cd5fb2a67
ratprogramer <samuelmartinezruiz43@gmail.com>
2024-04-20 11:03:33 -0500

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Integer me da funciones, int no
+        Integer pipi = 58;
+        System.out.println(pipi.floatValue());
+        System.out.println("Samuel Martinez Ruiz");
+    }
+
+}
